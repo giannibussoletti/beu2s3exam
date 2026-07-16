a5d834b0b2ff197ce70b61c0070e3887c98a8617
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-07-17 00:43:55 +0200

    Initial Commit

---
+++ src/main/java/gianni_bussoletti/beu2s3exam/Beu2s3examApplication.java
@@ -0,0 +1,13 @@
+package gianni_bussoletti.beu2s3exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beu2s3examApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beu2s3examApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gianni_bussoletti/beu2s3exam/Beu2s3examApplicationTests.java
@@ -0,0 +1,13 @@
+package gianni_bussoletti.beu2s3exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beu2s3examApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
